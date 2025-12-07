9aa13e3f1f91564ea3ccda7750c1a7454682c44b
James Woo <54916946+jameswoo245@users.noreply.github.com>
2025-12-06 19:14:31 -0800

    Delete

--- SLS Project/src/Visualizer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-
-class Visualizer {
-public:
-    // Warp src using dense flow (dx,dy per pixel)
-    static cv::Mat warpImage(const cv::Mat& src, const cv::Mat& flow);
-};
